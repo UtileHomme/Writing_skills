a9afaf73f7bca24aca28efca7a54fbaee3704dbd
UtileHomme <jatinsharma@healthheal.in>
2018-03-09 08:04:16 +0530

    Added some catchy sentences from an article

---
+++ Catchy_sentences/Some_Phrases.php
@@ -0,0 +1,19 @@
+<?php
+
+- Anxiety and doubt underminde promotion motivation, leaving you less likely to take any action at all
+
+- I know this doesn't sound like a barrel of laughs, particularly if you are usually more the promotion-minded type,
+but there is probably no better way to get over your anxiety about screwing up than to give some serious thought to all
+the dire consequences of doing nothing at all
+"barrel of laughs" - source of immense pleasure
+
+- Somewhere along the way, we've all bought into the idea - without consciously realizing it - that to be motivated and
+effective we need to feel like we want to take action.
+"buy into" - to start to do something that a lot of other people are doing
+
+- Studies show that people routinely overestimate their capacity for self-control, and rely on it too often to keep them
+out of hot water
+"in hot water" - in a troublesome or difficult situation
+
+
+ ?>
