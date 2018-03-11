8e258d2ee10faa91a864e8ad1b45a42d93450d64
UtileHomme <jatinsharma@healthheal.in>
2018-03-11 06:43:54 +0530

    More Sentences added

--- Catchy_sentences/Some_Phrases.php
+++ Catchy_sentences/Some_Phrases.php
@@ -1,10 +1,10 @@
 <?php
 
-- Anxiety and doubt underminde promotion motivation, leaving you less likely to take any action at all
+- Anxiety and doubt undermine promotion motivation, leaving you less likely to take any action at all.
 
 - I know this doesn't sound like a barrel of laughs, particularly if you are usually more the promotion-minded type,
 but there is probably no better way to get over your anxiety about screwing up than to give some serious thought to all
-the dire consequences of doing nothing at all
+the dire consequences of doing nothing at all.
 "barrel of laughs" - source of immense pleasure
 
 - Somewhere along the way, we've all bought into the idea - without consciously realizing it - that to be motivated and
@@ -14,6 +14,24 @@ effective we need to feel like we want to take action.
 - Studies show that people routinely overestimate their capacity for self-control, and rely on it too often to keep them
 out of hot water
 "in hot water" - in a troublesome or difficult situation
+
+- Do yourself a favour, and embrace the fact that your willpower is limited, and that it may not always be up to the challenge of
+getting you to do things you find difficult and tedious or otherwise awful.
+"be upto the challenge" - be able to act in response to a difficult situation
+
+- One of these bestsellers is Wilson's Letters to a Young Scientist, which distills 60 years of teaching into a wonderful array of
+unintuitive and surprising thoughts on creativity, innovation and scientific progress
+
+- You never finish a book as quite the same person, and - much like how bacteria inject and transfer fragments of DNA to one another -
+the best books transfer a part of the author's philosophy into your own head
+
+- He likes to probe, to collect, to tinker
+- He is not inclined to focus
+
+- They find little reward in the necessarily tedious chores of data-gathering and analysis
+- They choose not to take the hard roads to the frontier, over which the rest of us, the lesser intellectual toilers, must travel
+
+
 
 
  ?>
